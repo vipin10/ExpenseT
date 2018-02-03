8cda451ca424c558a7733fdf0e4dcb035171c037
vipin.rai <vipin1051992@gmail.com>
2018-02-03 16:36:17 +0530

    Initial commit

---
+++ app/src/main/java/android/com/vipin/expensetracker/FirstHome1.java
@@ -0,0 +1,13 @@
+package android.com.vipin.expensetracker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FirstHome1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first_home1);
+    }
+}
